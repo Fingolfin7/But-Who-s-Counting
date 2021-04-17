c3578b410919d75b81544a3fa06c41add4ea17cd
Henry Mushunje <mushunjek@gmail.com>
2021-04-18 00:26:34 +0200

    added haskeys.c part 12

--- But Who's Counting/init.c
+++ But Who's Counting/init.c
@@ -1,9 +1,38 @@
 #include "Definitions.h"
+#include "stdlib.h"
+
+//generate random 64 bit numbers / hash keys
+#define RAND_64  (  (U64)rand() +\
+					(U64)rand() << 15 +\
+					(U64)rand() << 30 +\
+					(U64)rand() << 45 +\
+					((U64)rand() & 0xF) << 60 )
 
 int Sq120ToSq64[BRD_SQ_NUM];
 int Sq64ToSq120[64];
 U64 SetMask[64];
 U64 ClearMask[64];
+
+U64 PieceKeys[13][120];
+U64 SideKey;
+U64 CastleKeys[16];
+
+void InitHashKeys() {
+	int index = 0;
+	int index2 = 0;
+
+	for (index = 0; index < 13; index++) {
+		for (index2 = 0; index2 < 120; index2++) {
+			PieceKeys[index][index2] = RAND_64;
+		}
+	}
+
+	SideKey = RAND_64;
+
+	for (index = 0; index < 16; index++) {
+		CastleKeys[index] = RAND_64;
+	}
+}
 
 void InitBitMasks() {
 	int index = 0;
@@ -48,5 +77,6 @@ void InitSq120to64() {
 void AllInit() {
 	InitSq120to64();
 	InitBitMasks();
+	InitHashKeys();
 }
 
